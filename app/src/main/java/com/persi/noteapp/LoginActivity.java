80b2f2ddead4e494c0eb96500db2df8c6dc3758a
parasg97 <parasg1997@gmail.com>
2018-07-10 17:36:11 +0530

    notification added 2

--- app/src/main/java/com/persi/noteapp/LoginActivity.java
+++ app/src/main/java/com/persi/noteapp/LoginActivity.java
@@ -5,9 +5,12 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,6 +20,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText mUserName;
     private TextInputEditText mPassword;
     private String mPref="UserDetail";
+    private String CHANNEL_ID="1";
+    private int notificationId=20;
 
 
     @Override
@@ -56,17 +61,27 @@ public class LoginActivity extends AppCompatActivity {
             editor.putString("name", mUserName.getText().toString());
             editor.putString("password", mPassword.getText().toString());
             editor.apply();
-            /*Intent intent = new Intent(this, ToDoList.class);
+            //setting up special activity which is not a part of regular UX flow
+      /*      Intent intent = new Intent(this, NotificationActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);*/
+      //this code is for accesing regular class from notification
+            Intent intent=new Intent(this,ToDoList.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            stackBuilder.addNextIntentWithParentStack(intent);
+            PendingIntent pendingIntent =
+                    stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
             createNotificationChannel();
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
                     .setSmallIcon(android.R.drawable.btn_star_big_on)
-                    .setContentTitle("Notification")
-                    .setContentText("Test Notification")
+                    .setContentTitle("Login details")
+                    .setContentText("Login Is Successful")
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                     .setContentIntent(pendingIntent)
-                    .setAutoCancel(true);*/
+                    .setAutoCancel(true);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            // notificationId is a unique int for each notification that you must define
+            notificationManager.notify(notificationId, mBuilder.build());
             Intent newIntent = new Intent(LoginActivity.this, ToDoList.class);
             finish();
             startActivity(newIntent);
@@ -74,19 +89,24 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    /*private void createNotificationChannel() {
+    private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
+            CharSequence name = "Test Channel";
+            String description = "Channel for testing";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
-    }*/
+    }
 }
